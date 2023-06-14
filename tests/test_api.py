0b46739bf6880a588d47c83c0f35319d241799c6
Visceros <propok63@gmail.com>
2023-06-14 12:25:06 +0300

    tests update

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,6 @@
 from django.urls import reverse
 from django.test.client import Client
+import requests
 
 
 def test_ping():
@@ -23,3 +24,33 @@ def test_auth_bad_creds():
         },
     )
     assert response.status_code == 401
+
+
+def test_api_connection():
+    client = Client()
+    response = client.post(
+        reverse("api:auth"),
+        content_type="application/json",
+        data = {"engine": "email",
+                "credentials": {
+                    "email": "super_admin@example.com",
+                    "password": "StrongPass777"
+                   }
+            }
+    )
+    content = response.json()
+    assert response.status_code == 200
+    assert content["user"]["first_name"] == "admin"
+
+
+# TODO дописать тесты
+#@pytest.mark.parametrize()
+# def test_user_registration():
+#     client = Client()
+#     response = client.post(
+#         reverse("api:auth"),
+#         content_type="application/json",
+#         data={
+#
+#         }
+#     )
